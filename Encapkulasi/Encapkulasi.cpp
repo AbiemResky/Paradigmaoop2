9ac2ea9644f6b095ac6b9204ead026c656440581
AbiemMuhammadResky <AbiemMuhammadResky@LAPTOP-63HSN1L1>
2024-05-27 10:30:54 +0700

    Add project files.

---
+++ Encapkulasi/Encapkulasi.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class remoteLampu {
+private:
+	string saklarNo[10];
+public:
+	void setsaklarNo(int i, string value) {
+		saklarNo[i] = value;
+	}
+	string getsaklarNo(int i) {
+		return saklarNo[i];
+
+	}
+};
+
+int main() {
+	remoteLampu lampurumah;
+
+	lampurumah.setsaklarNo(0, "lampu teras rumah");
+	lampurumah.setsaklarNo(1, "lampu ruang tamu");
+	lampurumah.setsaklarNo(2, "lampu kamar tidur");
+	lampurumah.setsaklarNo(3, "lampu dapur");
+
+	cout << lampurumah.getsaklarNo(0) << endl;
+	cout << lampurumah.getsaklarNo(1) << endl;
+	cout << lampurumah.getsaklarNo(2) << endl;
+	cout << lampurumah.getsaklarNo(3) << endl;
+	
+	return 0;
+}
